3bc8ccabf9e3da2bb1958fb6e672f7460d576ce6
ZDuygu <c1211040@student.cankaya.edu.tr>
2017-10-13 22:56:57 +0300

    Update myScript.ps1

--- myScript.ps1
+++ myScript.ps1
@@ -7,14 +7,22 @@ $services = Get-Service
 $getNetstat = netstat -n
 $sheduled =  Get-ScheduledTask | Get-ScheduledTaskInfo
 
-"" > 'text.txt'
+"#################### All Users ####################" > 'text.txt'
 Get-LocalUser >> 'text.txt'
+"#################### All Groups ####################" >> 'text.txt'
 Get-LocalGroup >> 'text.txt'
+"#################### Domain Name & User Name ####################" >> 'text.txt'
 $domain >> 'text.txt'
 $userName >> 'text.txt'
+"#################### User Info ####################" >> 'text.txt'
 $userInfo >> 'text.txt'
+"#################### System Info ####################" >> 'text.txt'
 $getSystemInfo >> 'text.txt'
+"#################### Computer Info ####################" >> 'text.txt'
 $computerInfo >> 'text.txt'
+"#################### All Services ####################" >> 'text.txt'
 $services >> 'text.txt'
+"#################### Netstat Output ####################" >> 'text.txt'
 $getNetstat >> 'text.txt'
+"#################### Sheduled Tasks ####################" >> 'text.txt'
 $sheduled >> 'text.txt'

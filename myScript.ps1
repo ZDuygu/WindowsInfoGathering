391a0703639d265d855fcaf55156aca8fd13dcbe
ZDuygu <c1211040@student.cankaya.edu.tr>
2017-10-13 22:48:25 +0300

    Update myScript.ps1

--- myScript.ps1
+++ myScript.ps1
@@ -1,20 +1,20 @@
+$domain = $env:UserDomain
+$userName = $env:UserName
+$userInfo = net accounts
 $getSystemInfo = Get-CimInstance Win32_OperatingSystem | Select-Object  Caption, InstallDate, ServicePackMajorVersion, OSArchitecture, BootDevice,  BuildNumber, CSName | FL
 $computerInfo = Get-ComputerInfo | Select-Object LogonServer, PowerPlatformRole, DeviceGuardSmartStatus, WindowsBuildLabEx, WindowsCurrentVersion, WindowsEditionId, WindowsInstallationType, WindowsInstallDateFromRegistry, WindowsProductId, WindowsProductName, WindowsSystemRoot ,WindowsVersion
-$name = $env:UserName
-$domain = $env:UserDomain
-$userInfo = net accounts
-$allGroup = Get-LocalGroup
-$allUsers = Get-LocalUser
+$services = Get-Service
 $getNetstat = netstat -n
 $sheduled =  Get-ScheduledTask | Get-ScheduledTaskInfo
 
 "" > 'text.txt'
+Get-LocalUser >> 'text.txt'
+Get-LocalGroup >> 'text.txt'
+$domain >> 'text.txt'
+$userName >> 'text.txt'
+$userInfo >> 'text.txt'
 $getSystemInfo >> 'text.txt'
 $computerInfo >> 'text.txt'
-$name >> 'text.txt'
-$domain >> 'text.txt'
-$userInfo >> 'text.txt'
-$allGroup >> 'text.txt'
-$allUsers >> 'text.txt'
+$services >> 'text.txt'
 $getNetstat >> 'text.txt'
 $sheduled >> 'text.txt'
